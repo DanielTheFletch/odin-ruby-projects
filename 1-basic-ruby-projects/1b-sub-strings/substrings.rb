44252829ba520f121ad4f4d6cdb6739bdf53d1ab
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-03-02 14:51:32 -0500

    Write substrings method

--- 1-basic-ruby-projects/1b-sub-strings/substrings.rb
+++ 1-basic-ruby-projects/1b-sub-strings/substrings.rb
@@ -1,3 +1,20 @@
 # Daniel Fletcher
 # The Odin Project: Ruby
-# Substrings
+# Sub Strings
+
+
+# Required function: Count total instances of each valid substring
+def substrings(string, possible)
+  # Convert all strings to lowercase for case-insensitive comparisons
+  lower_str = string.downcase
+  lower_arr = possible.map { |substring| substring.downcase }
+
+  # Retrieve all valid substrings in provided list
+  valid = possible.filter { |substring| lower.include?(substring.downcase) }
+
+  # Count times each valid substring appears
+  valid.reduce(Hash.new(0)) do |counts, substring|
+    lower.scan(substring.downcase) { |instance| counts[instance] += 1 }
+    counts
+  end
+end
