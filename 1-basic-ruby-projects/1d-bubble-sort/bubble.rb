a85772226600cc921aa49bfcbf8deef79f6e02cd
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-06-04 16:51:49 -0400

    1d: Implement #bubble_sort

--- 1-basic-ruby-projects/1d-bubble-sort/bubble.rb
+++ 1-basic-ruby-projects/1d-bubble-sort/bubble.rb
@@ -1,3 +1,20 @@
 # Daniel Fletcher
 # The Odin Project: Ruby
 # Bubble Sort
+
+
+def bubble_sort(values)
+    in_order = false
+
+    until in_order
+        in_order = true
+        for i in (0...values.size - 1) do
+            if values[i] > values[i + 1]
+                values[i], values[i + 1] = values[i + 1], values[i]
+                in_order = false
+            end
+        end
+    end
+
+    values
+end
