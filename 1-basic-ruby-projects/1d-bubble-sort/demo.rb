2e6ba81bb30fd6341fb1ce42349aef1d5d6af51e
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-06-04 17:03:32 -0400

    1d: Flesh out demo file

--- 1-basic-ruby-projects/1d-bubble-sort/demo.rb
+++ 1-basic-ruby-projects/1d-bubble-sort/demo.rb
@@ -9,3 +9,23 @@ require "./bubble.rb"
 puts "#bubble_sort demo"
 puts "-----------------"
 puts
+
+puts "[1] bubble_sort([4, 3, 78, 2, 0, 2])"
+puts "    => " << bubble_sort([4, 3, 78, 2, 0, 2]).to_s
+puts
+
+puts "[2] bubble_sort([17, 3, 6, 9, 15, 8, 6, 1, 10])"
+puts "    => " << bubble_sort([17, 3, 6, 9, 15, 8, 6, 1, 10]).to_s
+puts
+
+puts "[3] bubble_sort([-1, 1, -2, 2, -3, 3, 1234, 4321, 0])"
+puts "    => " << bubble_sort([-1, 1, -2, 2, -3, 3, 1234, 4321, 0]).to_s
+puts
+
+puts "[4] bubble_sort([99, 98, 97, 96, 95, 94, 93, 92, 91])"
+puts "    => " << bubble_sort([99, 98, 97, 96, 95, 94, 93, 92, 91]).to_s
+puts
+
+puts "[5] bubble_sort([2, 4, 6, 8])"
+puts "    => " << bubble_sort([2, 4, 6, 8]).to_s
+puts
