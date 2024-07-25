b6d464b35b57c8536bf4a8916be365827d1fc1a7
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-07-25 11:56:19 -0400

    2a: Update board functionality

--- 2-object-oriented-programming/2a-tic-tac-toe/board.rb
+++ 2-object-oriented-programming/2a-tic-tac-toe/board.rb
@@ -15,14 +15,35 @@
 # Check for a tie
 
 class Board
+
+  # Member data (read-only)
   attr_reader :board_state, :player_turn
 
   def initialize
-    @board_state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    @board_state = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     @player_turn = 0
+  end
+
+  def update_board(choice)
+    if valid_choice?(choice)
+      player_turn.even? ? value = 1 : value = -1
+      @board_state[choice - 1] = value
+      @player_turn += 1
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def valid_choice?(choice)
+    choice.between?(1, 9) && @board_state[choice - 1] == 0
   end
 end
 
 board = Board.new()
-puts "State: #{board.board_state}"
-puts " Turn: #{board.player_turn}"
+board.update_board(1)
+board.update_board(5)
+board.update_board(3)
+puts "Board State: #{board.board_state}"
