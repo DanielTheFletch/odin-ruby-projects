345bdcd10acb81b206982abde7c6a5cdc9679f62
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-07-30 12:02:40 -0400

    2b: Brainstorming and file structuring

---
+++ 2-object-oriented-programming/2b-mastermind/main.rb
@@ -0,0 +1,20 @@
+# Daniel Fletcher
+# The Odin Project: Ruby
+# Mastermind
+
+# Puzzle class
+# Initialize with correct solution
+# Track guesses
+# Encapsulate guess validation logic
+
+# Computer class
+# Parent class for DecoderCOM, EncoderCOM
+
+# EncoderCOM
+# Generates a code for guessing
+
+# DecoderCOM
+# Attempts to decode a code
+
+# Mastermind module
+# Encapsulates all relevant class logic
