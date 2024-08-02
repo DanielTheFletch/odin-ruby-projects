a4dc2b60186d498ecacc979ff03f8b162aeee53d
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-08-02 15:42:33 -0400

    2b: Organize and add comments

--- 2-object-oriented-programming/2b-mastermind/lib/mastermind/puzzle.rb
+++ 2-object-oriented-programming/2b-mastermind/lib/mastermind/puzzle.rb
@@ -1,3 +1,7 @@
+# Daniel Fletcher
+# The Odin Project: Ruby
+# Mastermind
+
 require 'colorize'
 
 module Mastermind
@@ -10,6 +14,7 @@ module Mastermind
       @max_guesses = max_guesses
     end
 
+    # Process a player's guess
     def process_guess(guess)
       @guesses += 1
       guess = code_to_colors(guess)
@@ -19,22 +24,7 @@ module Mastermind
 
     private
 
-    def code_to_colors(code)
-      code.upcase.split('').map { |char| char_to_color(char) }
-    end
-
-    def guess_to_string(guess)
-      guess_string = ""
-
-      guess.each { |color| guess_string << get_peg(color) << " " * 2 }
-
-      guess_string
-    end
-
-    def get_peg(color)
-      "O".colorize(color)
-    end
-
+    # Convert a character to a corresponding color symbol
     def char_to_color(char)
       case char
       when 'R' then :light_red
@@ -45,6 +35,26 @@ module Mastermind
       when 'W' then :light_white
       else nil
       end
+    end
+
+    # Convert a 4-char code into an array of symbols
+    # e.g., 'RGBR' -> [:light_red, :light_green, :light_cyan, :light_red]
+    def code_to_colors(code)
+      code.upcase.split('').map { |char| char_to_color(char) }
+    end
+
+    # Convert a guess to colored pegs as a string
+    def guess_to_string(guess)
+      guess_string = ""
+
+      guess.each { |color| guess_string << get_peg(color) << " " * 2 }
+
+      guess_string
+    end
+
+    # Retrieve the string representation of a colored peg
+    def get_peg(color)
+      "O".colorize(color)
     end
 
     # Validate code length
